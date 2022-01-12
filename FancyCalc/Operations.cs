83649f79a5ef92b5c8c688703273a9e06d9331bf
a91n0d <a91n0d@gmail.com>
2022-01-12 09:19:51 +0300

    Add project files.

---
+++ FancyCalc/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FancyCalc
+{
+    public static class Operations
+    {
+        public static int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+        public static int Sum(int x1, int x2, int x3)
+        {
+            int sum1 = x1 + x2;
+            int sum2 = x3;
+            int sum = sum1 + sum2;
+            return sum;
+        }
+    }
+}
